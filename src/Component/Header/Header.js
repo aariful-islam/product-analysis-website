cacf221d9817f0ae8f530bc98cf207d585962db4
arif99050 <arifulislam99050@gmail.com>
2022-04-04 01:19:51 +0600

    work with header(navbar)

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to="/">HOME</Link>
+                <Link to="/reviews">REVIEWS</Link>
+                <Link to="/dashboard">DASHBOARD </Link>
+                <Link to="/blogs">BLOGS</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;

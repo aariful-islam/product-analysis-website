d1aedf972a3a2ef5e10d5171006f54d555e6ec32
arif99050 <arifulislam99050@gmail.com>
2022-04-04 01:26:03 +0600

    set route of the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Component/Blogs/Blogs';
+import Dashboard from './Component/Dashboard/Dashboard';
 import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import NotFound from './Component/NotFound/NotFound';
+import Reviews from './Component/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
       
     </div>
   );
---
+++ src/Component/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>ERROR 404</h1>
+            <h1>No result found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
